b9116c9b48d6718f3d4de2e337a20687d4e39b46
spectralsun <garrett@spectralsun.com>
2015-08-15 13:31:58 -0700

    accept mimes and thumbnail size

--- configdist.py
+++ configdist.py
@@ -1,4 +1,9 @@
 DATABASE_URL = 'sqlite:///sqlite.db'
 
+IMAGE_ACCEPT_MIMES = ['image/jpeg', 
+                      'image/png', 
+                      'image/gif', 
+                      'image/x-ms-bmp']
+THUMBNAIL_SIZE = (90,90)                      
 UPLOAD_PATH = 'reactgur/static/uploads/'
 UPLOAD_WEB_PATH = 'static/uploads/'
