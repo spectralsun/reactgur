aa71e3aea0958853313acf0484fc494f30afc713
spectralsun <garrett@spectralsun.com>
2015-08-27 22:20:30 -0700

    upload config

--- configdist.py
+++ configdist.py
@@ -12,6 +12,9 @@ IMAGE_ACCEPT_MIMES = ['image/jpeg',
                       'image/png', 
                       'image/gif', 
                       'image/x-ms-bmp']
-THUMBNAIL_SIZE = (90,90)                      
+THUMBNAIL_SIZE = (90, 90)                      
 UPLOAD_PATH = 'reactgur/static/uploads/'
 UPLOAD_WEB_PATH = 'static/uploads/'
+UPLOAD_LOGIN_REQUIRED = True
+USER_MUST_BE_ENABLED = False
+USER_MUST_BE_VERIFIED = False
