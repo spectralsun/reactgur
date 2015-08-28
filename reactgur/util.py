e5a4d9c292fb274c82c13c42c24ce49798f8b392
spectralsun <garrett@spectralsun.com>
2015-08-28 15:20:18 -0700

    util file

--- reactgur/util.py
+++ reactgur/util.py
@@ -1,8 +1,19 @@
+import os
+import random
+
 import simplejson as json
 from flask import request, current_app
 from flask.json import dumps
+from PIL import Image, ImageOps, ImageChops
 from werkzeug import secure_filename
 
+
+image_extensions = {
+    'image/jpeg': '.jpg',
+    'image/png': '.png',
+    'image/gif': '.gif',
+    'image/x-ms-bmp': '.bmp'
+}
 
 class ExtensibleJSONEncoder(json.JSONEncoder):
     """A JSON encoder that returns the to_json method if present"""
@@ -12,9 +23,7 @@ class ExtensibleJSONEncoder(json.JSONEncoder):
         return super(ExtensibleJSONEncoder, self).default(obj)
 
 def jsonify(*args, **kwargs):
-    """
-    Returns a json response
-    """
+    """Returns a json response"""
     data = None
     indent = not request.is_xhr
     status = kwargs.pop('_status_code', 200)
@@ -31,6 +40,33 @@ def jsonify(*args, **kwargs):
         status=status,
         mimetype='application/json')    
 
+def convert_to_jpeg():
+    filename = generate_filename(upload_path, 
+        _image_extensions[mime])
+    newpath = upload_path + filename
+    im.convert('RGB').save(newpath, 'JPEG')
+    im = Image.open(newpath)
+    os.remove(filepath)
+
+def get_image_size(image):
+    im = Image.open(image)
+    return (im.size[0], im.size[1])
+
+def generate_thumbnail(file_path, save_path, thumbnail_size=(90, 90)):
+    im = Image.open(file_path)
+    thumbname = generate_filename(save_path, '.jpg')
+    thumb = ImageOps.fit(im, thumbnail_size, Image.ANTIALIAS, (0.5, 0.5))
+    if thumb.size[1] < thumbnail_size[1]:
+        thumb = im.crop((0, 0, thumbnail_size[0], thumbnail_size[1]))
+
+        offset_x = max((thumbnail_size[0] - im.size[0]) / 2, 0)
+        offset_y = max((thumbnail_size[1] - im.size[1]) / 2, 0)
+
+        thumb = ImageChops.offset(thumb, offset_x, offset_y)
+        thumb.convert('RGB')
+    thumb.save(os.path.join(save_path, thumbname))
+    return thumbname    
+
 def random_string(length=16):
     """Generates a random string containing a-z A-Z 0-9"""
     pool = range(48, 57) + range(65, 90) + range(97, 122)
@@ -41,6 +77,6 @@ def generate_filename(basepath, extension):
     exists = True
     while exists:
         filename = secure_filename(random_string() + extension)
-        path = basepath + filename
+        path = os.path.join(basepath, filename)
         exists = os.path.exists(path)
     return filename
