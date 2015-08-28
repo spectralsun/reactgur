8e3e9ae026f201e9dbe4ae404d32da9ba1c2ff18
spectralsun <garrett@spectralsun.com>
2015-08-28 15:46:00 -0700

    webpack file

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+/* eslint-env node */
+
+module.exports = {
+  entry: [
+    './client/reactgur.js'
+  ],
+  output: {
+    path: __dirname,
+    filename: 'reactgur/static/bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loaders: ['babel'],
+      },
+    ],
+  },
+  plugins: [
+  ]
+};
