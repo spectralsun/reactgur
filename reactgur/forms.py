b8469c4c5375bcede34ab75e9e4cb4b86e88947a
spectralsun <garrett@spectralsun.com>
2015-08-30 18:54:14 -0700

    registration and login forms

---
+++ reactgur/forms.py
@@ -0,0 +1,35 @@
+from wtforms import validators, HiddenField, PasswordField, StringField, \
+    TextField, BooleanField, ValidationError
+from wtforms.fields.html5 import EmailField
+from wtforms.widgets import TextArea
+from flask_wtf import Form
+
+from reactgur.models import User
+
+class RegistrationReqeustForm(Form):
+    email = EmailField('Email Address', [validators.DataRequired(), validators.Email()])
+    
+class RegistrationForm(Form):
+    username = TextField('Username', [validators.Length(min=4, max=25)])
+    email = EmailField('Email Address', [validators.DataRequired(), validators.Email()])
+    password = PasswordField('New Password', [
+        validators.Required(),
+        validators.Length(min=8),
+        validators.EqualTo('confirm', message='Passwords must match')
+    ])
+    confirm = PasswordField('Repeat Password')
+
+    def validate_username(form, field):
+        if User.get_user_by_username(field.data):
+            raise ValidationError('Username already registered.');
+
+    def validate_email(form, field):
+        if User.get_user_by_email(field.data):
+            raise ValidationError('Email already registered.')
+
+class TokenRegistrationForm(RegistrationForm):
+    token = TextField('token', [validators.DataRequired()])
+
+class LoginForm(Form):
+    username = TextField('Username', validators=[validators.DataRequired()])
+    password = TextField('Password', validators=[validators.DataRequired()])
