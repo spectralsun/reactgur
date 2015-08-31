4c783954880986dcdf30c088f8e95e7cb512ec70
spectralsun <garrett@spectralsun.com>
2015-08-31 02:05:59 -0700

    refactor gallery component

--- client/components/GalleryComponent.js
+++ client/components/GalleryComponent.js
@@ -1,10 +1,15 @@
 import React from 'react';
 
+import ee from '../Emitter.js';
 import scrollToPos from '../scrollToPos.js';
 
 export default class MediaComponent extends React.Component 
 {
-   componentDidMount() {
+    componentWillMount() {
+        this.setMaxSize(); 
+    }
+
+    componentDidMount() {
         if (this.iso)
             return;
 
@@ -26,7 +31,7 @@ export default class MediaComponent extends React.Component
         // relayout after reloading items
         this.iso.layout();
 
-        window.onresize = this.onWindowResize.bind(this);
+        ee.addListener('resize', this.onWindowResize.bind(this));
     }
 
     componentDidUpdate() {
@@ -44,7 +49,7 @@ export default class MediaComponent extends React.Component
 
     onLayoutComplete() {
         if (this.scrollPos) {
-            var item = this.expanded.link.parentNode;
+            var item = this.expanded.item;
             var height = item.getBoundingClientRect().height;
             var marginTop = parseInt(item.style.marginTop);
             var topPos = parseInt(item.style.top)
@@ -52,35 +57,29 @@ export default class MediaComponent extends React.Component
             if (this.expanded.img.height == this.max_height) 
                 scrollPos = topPos + marginTop - 4;
             scrollToPos(scrollPos);
+            this.scrollPos = false;
         }
     }
 
     onImageClick(e) {
-        if (this.expanded) {
-            this.expanded.img.src = this.expanded.link.dataset.thumbnail;
-            this.expanded.link.parentNode.style.marginLeft = '4px';
-            this.expanded.link.parentNode.style.marginTop = '4px';
-        }
-        this.expanded = {
+        var clicked = {
+            id: e.currentTarget.parentNode.dataset.id,
+            item: e.currentTarget.parentNode,
             link: e.currentTarget,
             img: e.target
         }
-        e.target.src = e.currentTarget.dataset.href;
+        if (this.expanded) {
+            this.expanded.img.src = this.expanded.link.dataset.thumbnail;
+            this.expanded.item.style.marginLeft = '4px';
+            this.expanded.item.style.marginTop = '4px';
+            if (this.expanded.id === clicked.id) {
+                this.expanded = null;
+                return;
+            }
+        }
+        this.expanded = clicked;
+        clicked.img.src = clicked.link.dataset.href;
         this.scrollPos = true;
-    }
-
-    setItemMargin() {
-        var item = this.expanded.link.parentNode;
-        var img = this.expanded.img;
-        var width = img.width + 16;
-        var height = img.height + 16;
-
-        if (img.width == this.max_width) {
-            item.style.marginLeft = '4px';
-        } else {
-            item.style.marginLeft = Math.floor((((Math.ceil(width / 198) * 198) - width) / 2)) + 'px';
-        } 
-        item.style.marginTop = (Math.floor((((Math.ceil(height / 198) * 198) - height) / 2)) + 4) + 'px';
     }
 
     onImageLoad() {
@@ -95,9 +94,27 @@ export default class MediaComponent extends React.Component
         this.updateImgStyles();
     }
 
+    setItemMargin() {
+        var item = this.expanded.item;
+        var img = this.expanded.img;
+        var width = img.width + 16;
+        var height = img.height + 16;
+
+        if (img.width == this.max_width) {
+            item.style.marginLeft = '4px';
+        } else {
+            item.style.marginLeft = Math.floor((((Math.ceil(width / 198) * 198) - width) / 2)) + 'px';
+        } 
+        item.style.marginTop = (Math.floor((((Math.ceil(height / 198) * 198) - height) / 2)) + 4) + 'px';
+    }
+
     setMaxSize() {
         this.max_width = 973;
         this.max_height = window.innerHeight - 92;
+        this.image_style = {
+            maxWidth: this.max_width,
+            maxHeight: this.max_height
+        }
     }
 
     updateImgStyles() {
@@ -115,46 +132,41 @@ export default class MediaComponent extends React.Component
         this.iso.layout();
     }
 
-    imgRef(img) {
-        this.img.push(img);
+    renderImage(image) {
+        return (
+            <div data-id={image.path} 
+                 key={image.path} 
+                 className="media-item well">
+                <a className="image ignore" 
+                   href="#"
+                   data-title={image.name} 
+                   data-thumbnail={image.thumbnail.href} 
+                   data-href={image.href}  
+                   onClick={this.onImageClick.bind(this)}>
+                    <img src={image.thumbnail.href} 
+                         style={this.image_style}
+                         ref={(i) => this.img.push(i)}
+                         onLoad={this.onImageLoad.bind(this)}  />
+                </a>
+            </div>
+        );
     }
 
     render() {
-        var images = this.props.images;
-        if (images.length == 0)
-            return (
-                <div className="jumbotron text-center">
-                    <h1 className="text-center">{"There's no images here!"}</h1>
-                    <p>Upload some images!</p>
-                    <p>
-                        <a href="/upload" className="btn btn-success btn-lg" >
-                            <span className="glyphicon glyphicon-cloud-upload"></span>
-                            <span> Upload Images</span>
-                        </a>
-                    </p>
-                </div>
-            )
-        this.img = [];
-        this.setMaxSize();
-        var mediaItems = [];
-        var image_style = {
-            maxWidth: this.max_width,
-            maxHeight: this.max_height
-        }
-        for(var x = 0; x < images.length; x++) {
-            var image = images[x];
-            var img = <img src={image.thumbnail.href} ref={this.imgRef.bind(this)} onLoad={this.onImageLoad.bind(this)} style={image_style} />;
-            mediaItems.push(
-                <div key={image.path} className="media-item well">
-                    <a className="image ignore" data-title={image.name} data-thumbnail={image.thumbnail.href} data-href={image.href} href="#" onClick={this.onImageClick.bind(this)}>
-                        {img}
+        return this.props.images.length == 0 ? (
+            <div className="jumbotron text-center">
+                <h1 className="text-center">{"There's no images here!"}</h1>
+                <p>Upload some images!</p>
+                <p>
+                    <a href="/upload" className="btn btn-success btn-lg" >
+                        <span className="glyphicon glyphicon-cloud-upload"></span>
+                        <span> Upload Images</span>
                     </a>
-                </div>
-            );
-        }
-        return (
+                </p>
+            </div> 
+        ) : (
             <div ref="isoContainer" className="panel media-component text-center">
-                {mediaItems}
+                {this.props.images.map(this.renderImage.bind(this))}
             </div>
         );
     }
