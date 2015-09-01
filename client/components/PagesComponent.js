82d51c3703177c59e20df90de4787be55d37e98f
spectralsun <garrett@spectralsun.com>
2015-08-31 20:33:05 -0700

    resize page wrapper

--- client/components/PagesComponent.js
+++ client/components/PagesComponent.js
@@ -10,16 +10,33 @@ export default class PagesComponent extends React.Component
         super(props);
         this.last_path = '/';
         ee.addListener('modal_close', this.onModalClose.bind(this)); 
+        ee.addListener('resize', this.handleResize.bind(this));
+    }
+
+    componentDidMount() {
+        this.refs.page_wrapper.getDOMNode().style.height = (window.innerHeight - 51) + 'px';
+        ee.emit('page_wrapper', this.refs.page_wrapper);
     }
     
     onModalClose(modal) {
         history.pushState({}, '', this.last_path);
     }
 
+    handleResize() {
+        if (this.refs.page_wrapper)
+            this.refs.page_wrapper.getDOMNode().style.height = (window.innerHeight - 51) + 'px';
+    }
+
+    handleScroll(e) {
+        ee.emit('wrapper_scroll', e);
+    }
+
     render() {
         return (
-            <div id="page" className="container">
-                 <HomePage ref="homePage" />
+            <div id='page_wrapper' ref='page_wrapper' onScroll={this.handleScroll.bind(this)}>
+                <div id="page" className="container">
+                     <HomePage ref="homePage" />
+                </div>
             </div>
         );
     }
