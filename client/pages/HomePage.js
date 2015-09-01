d360c27ab161a50082b0290d7bac51bf5cb010d6
spectralsun <garrett@spectralsun.com>
2015-08-31 20:33:59 -0700

    set gallery component state

--- client/pages/HomePage.js
+++ client/pages/HomePage.js
@@ -1,13 +1,23 @@
 import React from 'react';
 
 import GalleryComponent from '../components/GalleryComponent.js';
-
+import ee from './../Emitter.js';
 
 export default class HomePage extends React.Component 
-{   
-    componentDidMount() {
-        this.refs.galleryComponent.setState({images: window.IMAGES})
+{  
+    constructor(props) {
+        super(props);
+        ee.addListener('media_uploaded', this.handleMediaUploaded.bind(this));
     } 
+
+    componentDidMount() {
+        this.refs.galleryComponent.setState({images: IMAGES});
+    }
+
+    handleMediaUploaded(media) {
+        this.refs.galleryComponent.prependMedia(media)
+    } 
+    
     render() {
         return (
             <GalleryComponent ref='galleryComponent' />
